c46255fc92b600e2f344f7167b52040bd8717d2a
Wonjun Lee <jasonw0416@gmail.com>
2022-03-26 16:53:52 -0700

    read input to set alarm time

--- combined/combined.ino
+++ combined/combined.ino
@@ -24,6 +24,81 @@ int dur[] = {2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 2
 int duration = 100; // 500 milliseconds
 size_t size = sizeof(melody) / sizeof(melody[0]);
 
+
+/*#include <Keypad.h>
+//four rows
+const byte ROWS = 4;
+//four columns
+const byte COLS = 4;
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+{'1','2','3','A'},
+{'4','5','6','B'},
+{'7','8','9','C'},
+{'*','0','#','D'}
+};
+//connect to the row pinouts of the keypad
+byte rowPins[ROWS] = {9, 8, 7, 6};
+//connect to the column pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2};
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+String password = "ABC123";
+String input = "";*/
+
+
+int hour = 0;
+int minute = 0;
+int second = 0;
+bool check = false;
+
+void alarm_init(){
+  Serial.println("Type Hour");
+  while (!check){
+    while (Serial.available() == 0) { } 
+    String hour_string = Serial.readStringUntil('\n');
+    hour = hour_string.toInt();
+    if (hour >= 0 && hour < 23){
+      Serial.print("Hour set: ");
+      Serial.println(hour);
+      check = true;
+    } else { 
+      Serial.println("Invalid input");
+    }
+  }
+  check = false;
+
+  Serial.println("Type Minute");
+  while (!check){
+    while (Serial.available() == 0) { } 
+    String min_string = Serial.readStringUntil('\n');
+    minute = min_string.toInt();
+    if (minute < 0 || minute > 59){
+      Serial.println("Invalid input");
+    } else { 
+      Serial.print("Minute set: ");
+      Serial.println(minute);
+      check = true;
+    }
+  }
+  check = false;
+
+  Serial.println("Type Second");
+  while (!check){
+    while (Serial.available() == 0) { } 
+    String sec_string = Serial.readStringUntil('\n');
+    second = sec_string.toInt();
+    if (second < 0 || second > 59){
+      Serial.println("Invalid input");
+    } else { 
+      Serial.print("Second set: ");
+      Serial.println(second);
+      check = true;
+    }
+  }
+}
+
+
 void setup ()
 {
   Serial.begin(9600);
@@ -45,6 +120,8 @@ void setup ()
     // for example to set January 27 2017 at 12:56 you would call:
     // rtc.adjust(DateTime(2017, 1, 27, 12, 56, 0));
   }
+
+  alarm_init();
 }
 
 void loop () {
@@ -79,36 +156,20 @@ void loop () {
     
     Serial.println();
 /*
-  for (int thisNote = 0; thisNote < size; thisNote++) {
+    char customKey = customKeypad.getKey();
+  
+    if (customKey){
+      if (customKey == '*'){
+        input = "";
+      }
+      else {
+        input += customKey;
+        Serial.println(input);
+      }
+    }
+    
+    if (input == password){
+      Serial.println("Unlocked!");
+    }*/
 
-    DateTime now = rtc.now();
-
-    Serial.println("Current Date & Time: ");
-
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(11, melody[thisNote], 200);
-    // Output the voice after several minutes
-    delay(dur[thisNote] * 200);
-    //delay(250);
-    DateTime future (now - TimeSpan(1, -9, 10, 30));
-
-    // Serial.println("Future Date & Time (Now + 7days & 30s): ");
-    Serial.print(future.year(), DEC);
-    Serial.print('/');
-    Serial.print(future.month(), DEC);
-    Serial.print('/');
-    Serial.print(future.day(), DEC);
-    Serial.print(' ');
-    Serial.print(future.hour(), DEC);
-    Serial.print(':');
-    Serial.print(future.minute(), DEC);
-    Serial.print(':');
-    Serial.print(future.second(), DEC);
-    Serial.println();
-
-    Serial.println();
-
-  }
-  // restart after two seconds
-  delay (400);*/
 }
